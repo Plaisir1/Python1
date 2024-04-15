552be6936d0dd921a0042b0184a3a9763ada8a8d
Plaisir1 <mwizerwaaime9@gmail.com>
2024-04-15 10:14:26 +0200

    Added python files

---
+++ conditions.py
@@ -0,0 +1,10 @@
+n = int(input("Enterany number: "))
+
+if n > 0:
+    print(f"{n}, is a positive number")
+
+elif n < 0:
+    print(f"{n}, is a negative number")
+
+else:
+    print("The number you enetered is zero")
